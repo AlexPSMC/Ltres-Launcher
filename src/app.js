ad9bf1b78c3285f3591e02f92a7e5f03a272d716
AlexPSMC <200976721+AlexPSMC@users.noreply.github.com>
2025-12-14 18:27:57 -0500

    2.2.1.8

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const RPC = require('discord-rpc');
 const UpdateWindow = require("./assets/js/windows/updateWindow.js");
 const MainWindow = require("./assets/js/windows/mainWindow.js");
 
-const CLIENT_ID = '1444191237614866432';
+const CLIENT_ID = '1443406019740041257';
 RPC.register(CLIENT_ID);
 
 const rpc = new RPC.Client({ transport: 'ipc' });
